cb775aa57732061350b11cca72a29a85f874244a
Monark Basak <monarkb01@gmail.com>
2024-03-21 09:20:41 -0400

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mbasak99/lets-go
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Start server on :3001")
+	err := http.ListenAndServe(":3001", mux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
