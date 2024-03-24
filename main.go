b112af0557d11f933c87a658ae9543e3e53e37ee
Monark Basak <monarkb01@gmail.com>
2024-03-24 12:14:55 -0400

    Added restrictions to only allow POST calls for snippetCreate and added query params for snippetView.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,16 +16,31 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		// Header().Set() method to allow POST calls only
+		// Adds "Allow: POST" to the response header map
+		// Call this before WriteHeader and/or Write, otherwise it has no effect
+		w.Header().Set("Allow", http.MethodPost)
 		// Reject any request that's not POST and send a 405 Status (Method not allowed)
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		/* w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed")) */
+		// http.Error() calls WriteHeader and Write behind the scenes
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	// If you don't call WriteHeader() before calling Write() it automatically
+	// sends back a 200 (OK)
 	w.Write([]byte("Create a new snippet..."))
 }
 
