2e32179a6d7ab8dcc7eda4de71d0d3338d2355fe
Monark Basak <monarkb01@gmail.com>
2024-05-13 13:14:05 -0400

    Added custom logging for web server and modified the server a bit.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -16,6 +17,14 @@ func main() {
 	// a value, and need to parse before it's used anywhere or it'll default
 	// to 3001.
 	flag.Parse()
+
+	// Create custom loggers to provide standard info.
+	// The pipe (|) is used as a join (bitwise OR).
+	// NOTE:
+	// Custom loggers are also concurrency-safe so it's safe to share them
+	// across multiple go-routines or handlers without fear of race conditions.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -30,10 +39,19 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Start server on http://localhost%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	// By default http uses the standard logger, so I need to manually use mine
+	// Also set the port address and handler to use for the server, allowing me
+	// to remove the *addr and mux parameters in the ListenAndServe() call.
+	server := http.Server{
+		Addr:     *addr,
+		ErrorLog: errLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on http://localhost%s", *addr)
+	err := server.ListenAndServe()
 
 	if err != nil {
-		log.Fatal(err)
+		errLog.Fatal(err)
 	}
 }
